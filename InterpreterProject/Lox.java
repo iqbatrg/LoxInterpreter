1a91fa155f668f44204d5fecc98faa6a070d8504
iqbatrg <109561812+iqbatrg@users.noreply.github.com>
2024-09-03 22:42:15 -0500

    First commit
    Adding framework file

--- InterpreterProject/Lox.java
+++ InterpreterProject/Lox.java
@@ -9,8 +9,27 @@ import java.nio.file.Paths;
 import java.util.List;
 
 
+
 public class Lox {
-    public static void main(String[] args) throws IOException {
-        if(args.length > 1)
+
+    //runFile method that is used to run text files passed to lox
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
     }
-}
+
+    //lox main method for running lox
+    public static void main(String[] args) throws IOException {
+        if(args.length > 1) { //what to do if the arguement is invalid
+            System.out.println("Usage: Jlox [script]");
+            System.exit(64);
+        } else if (args.length) { //what to do if the argument is a filepath leading to a file with code
+            runFile(args[0]);
+        }
+        else{
+            runPrompt();
+        }
+
+     }
+    
+ }
