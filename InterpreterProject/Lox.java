3b63527f6bbd7a08c2bfa6e8a30dbb9acb8c07b9
iqbatrg <109561812+iqbatrg@users.noreply.github.com>
2024-09-04 11:47:41 -0500

    Added a few functions, run

--- InterpreterProject/Lox.java
+++ InterpreterProject/Lox.java
@@ -18,6 +18,31 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
     }
 
+
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        //printing tokens?
+        for(Token token : tokens)
+        {
+            System.out.println(token);
+        }
+    }
+
+
+    //runPrompt function that runs code in the interactive interface
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input); //
+
+        for(;;) {
+            System.out.print(">"); //forever loop
+            String line = reader.readLine();
+            if(line == null) break;
+            run(line);
+        }
+    } 
     //lox main method for running lox
     public static void main(String[] args) throws IOException {
         if(args.length > 1) { //what to do if the arguement is invalid
