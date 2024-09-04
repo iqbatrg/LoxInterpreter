d9afefab5c50e5e9a1b1c0d9f300dd710bf90546
iqbatrg <109561812+iqbatrg@users.noreply.github.com>
2024-09-03 21:05:30 -0500

    Added first file

---
+++ InterpreterProject/Lox.java
@@ -0,0 +1,16 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        if(args.length > 1)
+    }
+}
